abfc794da57f790028707790afd8b22fb9f02d1a
ritacserra <20240515@novaims.unl.pt>
2025-04-25 20:48:28 +0100

    Seating Arrangement Class

---
+++ SeatingArrangement.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class SeatingArrangement:
+
+    def __init__ (self, assignments, relations_mtx):
+        """
+        Initializes a seating arrangement.
+        
+        Parameters:
+        - assignment: A numpy array of length 64, where each index represents a guest
+                      and the value is the assigned table (0 to 7).
+        - relations_mtx: A 64x64 numpy array with the pairwise relationship scores.
+        """
+        self.assignments = assignments 
+        self.relations_mtx = relations_mtx
+        self.fitness = self.fitness_table()
+    
+    def fitness_table(self):
+        """
+        Calculates the fitness of the current seating arrangement.
+        The fitness is the total sum of relationship scores for all guest pairs seated at the same table.
+
+        Returns:
+        - total_fitness (int): The total happiness score of the current arrangement.
+        """
+
+        total_fitness=0
+
+        for table in range (8):
+
+            # Get all guests assigned to the current table
+            guests_at_table = np.where(self.assignments == table)[0]
+
+            # Sum the relationship scores for all unique pairs at this table
+            for i in range(len(guests_at_table)):
+
+                for j in range(i + 1, len(guests_at_table)):
+                    a = guests_at_table[i]
+                    b = guests_at_table[j]
+                    total_fitness += self.relations_mtx[a][b]
+        
+        return total_fitness
+
